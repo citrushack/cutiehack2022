7f0d18cbb8331d2ce44216190aa282e79f45d557
theo <theobfernandez@gmail.com>
2022-07-22 10:21:18 -0700

    Add fixes to 500 page

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const error500: React.FC = () => {
+export default function error500()
+{
   return (
-    <div className="h-screen p-16 flex flex-col px-5 mx-auto justify-center items-center">
-      <h1 className="mb-10 font-extrabold text-8xl">500</h1>
-      <p className="font-semibold text-2xl">Uh oh. Something broke on our end. Check back later.</p>
-    </div>
+    <section className="h-screen p-16 flex flex-col px-5 mx-auto justify-center items-center">
+      <h1>500</h1>
+      <p>Uh oh. Something broke on our end. Check back later.</p>
+    </section>
   );
 };
-
-export default error500
