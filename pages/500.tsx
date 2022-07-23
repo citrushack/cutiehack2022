e6473bacefbef4a4f28bf6dba2bed3142218313c
theo <theobfernandez@gmail.com>
2022-07-22 20:06:27 -0700

    Fix formatting and text align on smaller screen sizes

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { Page } from '@/components/Page/Page'
+import React from "react";
+import { Page } from "@/components/Page/Page";
+import styles from "../styles/Home.module.css";
 
-export default function error500()
-{
+export default function error500() {
   return (
     <Page>
-      <section className="h-screen p-16 flex flex-col px-5 mx-auto justify-center items-center">
+      <section className="p-16 flex flex-col justify-center items-center text-center">
         <h1>500</h1>
-        <p>Uh oh. Something broke on our end. Check back later.</p>
+        <p>
+          Uh oh. Something broke on our end. Check back later.
+        </p>
       </section>
     </Page>
   );
-};
+}
