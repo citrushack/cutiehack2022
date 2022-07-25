16b32f2f2b8307eea0c0241e64f2b518809282b6
Minsoo Kim <minsooerickim@gmail.com>
2022-07-25 16:35:37 -0700

    fix padding & prettier

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import { Page } from "@/components/Page/Page";
-import styles from "../styles/Home.module.css";
+import React from 'react'
+import { Page } from '@/components/Page/Page'
+import styles from '../styles/Home.module.css'
 
 export default function error500() {
   return (
     <Page>
-      <section className="p-16 flex flex-col justify-center items-center text-center">
+      <section className="p-4 flex flex-col justify-center items-center text-center">
         <h1>500</h1>
-        <p>
-          Uh oh. Something broke on our end. Check back later.
-        </p>
+        <p>Uh oh. Something broke on our end. Check back later.</p>
       </section>
     </Page>
-  );
+  )
 }
