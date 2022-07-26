889b896e4ac8320365a8c85a559a46bb14091ac7
NateM135 <melwaninate@gmail.com>
2022-07-25 19:44:29 -0700

    github oauth added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,10 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import NextAuth from 'next-auth'
-import EmailProvider from 'next-auth/providers/email'
-import GoogleProvider from 'next-auth/providers/google'
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-import clientPromise from 'lib/mongodb'
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "lib/mongodb";
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
   return await NextAuth(req, res, {
@@ -14,6 +15,10 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+      GithubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
       }),
       EmailProvider({
         server: {
@@ -29,31 +34,37 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
     ],
     callbacks: {
       async signIn({ user, account, profile }) {
-        if (account.provider === 'google') {
+        if (account.provider === "google") {
           // first and last name attributes are available for GoogleProfile
           // -- https://github.com/nextauthjs/next-auth/blob/main/packages/next-auth/src/providers/google.ts
           user.name = {
             first: String(profile.given_name),
             last: String(profile.family_name),
-          }
+          };
         }
-        return true
+        if (account.provider === "github") {
+          user.name = {
+            first: null,
+            last: null,
+          };
+        }
+        return true;
       },
       async session({ session, user }) {
-        session.user.uid = user.uid
-        session.user.gid = user.gid
-        session.user.name = user.name
-        session.user.qualified = user.qualified
-        session.user.admin = user.admin
-        session.user.checkedIn = user.checkedIn
-        session.user.participation = user.participation
-        return session
+        session.user.uid = user.uid;
+        session.user.gid = user.gid;
+        session.user.name = user.name;
+        session.user.qualified = user.qualified;
+        session.user.admin = user.admin;
+        session.user.checkedIn = user.checkedIn;
+        session.user.participation = user.participation;
+        return session;
       },
     },
     pages: {
-      verifyRequest: '/verify-signin', // Used for check email page
-      newUser: '/', // Redirect new users to apply (replace to '/' when apps close)
+      verifyRequest: "/verify-signin", // Used for check email page
+      newUser: "/", // Redirect new users to apply (replace to '/' when apps close)
     },
     secret: process.env.SECRET,
-  })
+  });
 }
