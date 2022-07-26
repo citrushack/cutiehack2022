e8b8a308810859cf0543804c3f7a9b5ed9554d79
Minsoo Kim <minsooerickim@gmail.com>
2022-07-25 20:48:16 -0700

    prettier changes

--- pages/api/applications/manual-review.ts
+++ pages/api/applications/manual-review.ts
@@ -3,7 +3,10 @@ import clientPromise from '@/lib/mongodb'
 import { sendEmail } from '@/lib/sendgrid'
 import { getSession } from 'next-auth/react'
 
-export default async function manualReviewApplication(req: NextApiRequest, res: NextApiResponse) {
+export default async function manualReviewApplication(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const session = await getSession({ req })
   const db = (await clientPromise).db(process.env.MONGODB_DB)
   if (session && session.user.admin) {
@@ -18,32 +21,28 @@ export default async function manualReviewApplication(req: NextApiRequest, res: 
           name: users[i].name.first,
           members: '',
           invite_code: '',
-          newcomer: ''
+          newcomer: '',
         })
-        await db.collection('users').updateOne(
-          { uid: users[i].uid },
-          { $set: { qualified: 'yeah' } }
-        )
-      }
-      else {
+        await db
+          .collection('users')
+          .updateOne({ uid: users[i].uid }, { $set: { qualified: 'yeah' } })
+      } else {
         await sendEmail({
           email: users[i].email,
           template_id: process.env.APP_REJECTED_EMAIL_ID,
           name: users[i].name.first,
           members: '',
           invite_code: '',
-          newcomer: ''
+          newcomer: '',
         })
-        await db.collection('users').updateOne(
-          { uid: users[i].uid },
-          { $set: { qualified: 'nope' } }
-        )
+        await db
+          .collection('users')
+          .updateOne({ uid: users[i].uid }, { $set: { qualified: 'nope' } })
       }
     }
-  
+
     res.status(200).json({})
-  }
-  else {
+  } else {
     res.status(401).json({})
   }
 }
