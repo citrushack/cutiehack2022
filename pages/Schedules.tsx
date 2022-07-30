36294c837b411e7653887041e74f4db2905972be
Nicholas Chao <nibchao@gmail.com>
2022-07-30 12:26:53 -0700

    added more formatting kinda looks like current website

--- pages/Schedules.tsx
+++ pages/Schedules.tsx
@@ -4,36 +4,42 @@ import React from 'react'
 export default function Schedules() {
   return (
     <Page>
-        <section className="flex flex-col text-center">
+        <section className="flex flex-col text-center w-full px-12">
             <h1>Schedule</h1>
             <h2>Schedule is in PST (Pacific Standard Time)</h2>
             <h3>Saturday, April 2</h3>
-              <div className="flex flex-row">
-                <div className="flex flex-col w-full">Events
-                  <div className="flex flex-col">
-                    <div className="flex flex-col">
-                      item1
+              <div className="flex flex-row gap-5 px-12 my-6">
+                <div className="flex flex-col w-full gap-5">
+                  <div className="flex flex-col">Events</div>
+                    <div className="flex items-center gap-5">
+                    <div className="flex flex-col w-full text-left">7 - 8 AM</div>
+                    <div className="flex flex-col w-full text-left">
+                      <div>Check-In</div>
+                      <div>Hybrid</div>
+                      <div>(In Person @ MSE 116)</div>
                     </div>
-                    <div className="flex flex-col">
-                      item1
+                  </div> 
+                </div>
+                <div className="flex flex-col w-full gap-5">
+                  <div className="flex flex-col">Workshops</div>
+                    <div className="flex items-center gap-5">
+                    <div className="flex flex-col w-full text-left">9:30 - 10 AM</div>
+                    <div className="flex flex-col w-full text-left">
+                      <div>Teambuilding Activity</div>
+                      <div>Hybrid</div>
+                      <div>(In Person @ MSE 116)</div>
                     </div>
-                  </div>
-                </div> 
-                <div className="flex flex-col w-full">Workshops
-                  <div className="flex flex-col">
-                    item2
-                  </div>
-                  <div className="flex flex-col">
-                      item2
-                  </div>
-                </div> 
-                <div className="flex flex-col w-full">Activities
-                  <div className="flex flex-col">
-                    item3
-                  </div>
-                  <div className="flex flex-col">
-                      item3
-                  </div>
+                  </div> 
+                </div>
+                <div className="flex flex-col w-full gap-5">
+                  <div className="flex flex-col">Activities</div>
+                    <div className="flex items-center gap-5">
+                    <div className="flex flex-col w-full text-left">12 PM - 12 AM</div>
+                    <div className="flex flex-col w-full text-left">
+                      <div>CTF</div>
+                      <div>Via Cyber@UCR Website</div>
+                    </div>
+                  </div> 
                 </div>
               </div>
         </section>
