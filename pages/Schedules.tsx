8d9249851688822e3310adb93d5569fad2122c3a
Nicholas Chao <nibchao@gmail.com>
2022-07-30 09:43:11 -0700

    added barebones flex for schedule items

--- pages/Schedules.tsx
+++ pages/Schedules.tsx
@@ -6,12 +6,36 @@ export default function Schedules() {
     <Page>
         <section className="flex flex-col text-center">
             <h1>Schedule</h1>
-            <p>Schedule is in PST (Pacific Standard Time)</p>
-            <div className="flex flex-col items-center">
-              <div className="flex flex-col">test</div>
-              <div className="flex flex-col">test2</div>
-              <div className="flex flex-col">test3</div>
-            </div>
+            <h2>Schedule is in PST (Pacific Standard Time)</h2>
+            <h3>Saturday, April 2</h3>
+              <div className="flex flex-row">
+                <div className="flex flex-col w-full">Events
+                  <div className="flex flex-col">
+                    <div className="flex flex-col">
+                      item1
+                    </div>
+                    <div className="flex flex-col">
+                      item1
+                    </div>
+                  </div>
+                </div> 
+                <div className="flex flex-col w-full">Workshops
+                  <div className="flex flex-col">
+                    item2
+                  </div>
+                  <div className="flex flex-col">
+                      item2
+                  </div>
+                </div> 
+                <div className="flex flex-col w-full">Activities
+                  <div className="flex flex-col">
+                    item3
+                  </div>
+                  <div className="flex flex-col">
+                      item3
+                  </div>
+                </div>
+              </div>
         </section>
     </Page>
   )
