e71f43d4872bccd1d25bf4a84c72f951b28234d8
Nicholas Chao <nibchao@gmail.com>
2022-07-30 14:12:01 -0700

    minor formatting + added second day

--- pages/Schedules.tsx
+++ pages/Schedules.tsx
@@ -6,46 +6,76 @@ export default function Schedules() {
     <Page>
         <section className="flex flex-col text-center w-full px-12">
             <h1>Schedule</h1>
-            <h2>Schedule is in PST (Pacific Standard Time)</h2>
-            <h3>Saturday, April 2</h3>
-              <div className="flex flex-row gap-5 px-12 my-6">
-                <div className="flex flex-col w-full gap-5">
-                  <div className="flex flex-col">Events</div>
-                    <div className="flex items-center gap-5">
-                    <div className="flex flex-col w-full text-left">7 - 8 AM</div>
-                    <div className="flex flex-col w-full text-left">
-                      <div>Check-In</div>
-                      <div>Hybrid</div>
-                      <div>(In Person @ MSE 116)</div>
-                    </div>
-                  </div> 
-                </div>
-                <div className="flex flex-col w-full gap-5">
-                  <div className="flex flex-col">Workshops</div>
-                    <div className="flex items-center gap-5">
-                    <div className="flex flex-col w-full text-left">9:30 - 10 AM</div>
-                    <div className="flex flex-col w-full text-left">
-                      <div>Teambuilding Activity</div>
-                      <div>Hybrid</div>
-                      <div>(In Person @ MSE 116)</div>
-                    </div>
-                  </div> 
-                </div>
-                <div className="flex flex-col w-full gap-5">
-                  <div className="flex flex-col">Activities</div>
-                    <div className="flex items-center gap-5">
-                    <div className="flex flex-col w-full text-left">12 PM - 12 AM</div>
-                    <div className="flex flex-col w-full text-left">
-                      <div>CTF</div>
-                      <div>
-                        <a href="https://ctf.ucrcyber.org/">
-                          <span className="hover:underline">Via Cyber@UCR Website</span>
-                        </a>
-                      </div>
-                    </div>
-                  </div> 
+            <h3>Schedule is in PST (Pacific Standard Time)</h3>
+            <h2>Saturday, April 2</h2>
+            <div className="flex flex-row gap-5 px-12 my-6">
+              <div className="flex flex-col w-full gap-5">
+                <div className="flex flex-col font-medium">Events</div>
+                <div className="flex items-center gap-5 p-5">
+                  <div className="flex flex-col w-full text-left font-medium">7 - 8 AM</div>
+                  <div className="flex flex-col w-full text-left">
+                    <div className="font-medium">Check-In</div>
+                    <div>Hybrid</div>
+                    <div>(In Person @ MSE 116)</div>
+                  </div>
                 </div>
               </div>
+              <div className="flex flex-col w-full gap-5">
+                <div className="flex flex-col font-medium">Workshops</div>
+                <div className="flex items-center gap-5 p-5">
+                  <div className="flex flex-col w-full text-left font-medium">9:30 - 10 AM</div>
+                  <div className="flex flex-col w-full text-left">
+                    <div className="font-medium">Teambuilding Activity</div>
+                    <div>Hybrid</div>
+                    <div>(In Person @ MSE 116)</div>
+                  </div>
+                </div> 
+                <div className="flex items-center gap-5 p-5">
+                  <div className="flex flex-col w-full text-left font-medium">10 - 11 AM</div>
+                  <div className="flex flex-col w-full text-left">
+                    <div className="font-medium">Intro to Git/GitHub</div>
+                    <div>Hybrid</div>
+                    <div>(In Person @ Bourns A265)</div>
+                  </div>
+                </div> 
+              </div>
+              <div className="flex flex-col w-full gap-5">
+                <div className="flex flex-col font-medium">Activities</div>
+                <div className="flex items-center gap-5 p-5">
+                  <div className="flex flex-col w-full text-left font-medium">12 PM - 12 AM</div>
+                  <div className="flex flex-col w-full text-left">
+                    <div className="font-medium">CTF</div>
+                    <div>
+                      <a href="https://ctf.ucrcyber.org/">
+                        <span className="hover:underline">Via Cyber@UCR Website</span>
+                      </a>
+                    </div>
+                  </div>
+                </div> 
+              </div>
+            </div>
+            <h2>Sunday, April 3</h2>
+            <div className="flex flex-row gap-5 px-12 my-6">
+              <div className="flex flex-col w-full gap-5">
+                <div className="flex flex-col font-medium">Events</div>
+                <div className="flex items-center gap-5 p-5">
+                  <div className="flex flex-col w-full text-left font-medium">8 AM - 1 PM</div>
+                  <div className="flex flex-col w-full text-left">
+                    <div className="font-medium">Red Bull Giveaway</div>
+                    <div>In-Person @ WCH 205/206</div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col w-full gap-5">
+                <div className="flex flex-col font-medium">Activities</div>
+                <div className="flex items-center gap-5 p-5">
+                  <div className="flex flex-col w-full text-left font-medium">9 - 10 AM</div>
+                  <div className="flex flex-col w-full text-left">
+                    <div className="font-medium">Creative Building on Minecraft</div>
+                  </div>
+                </div> 
+              </div>
+            </div>
         </section>
     </Page>
   )
