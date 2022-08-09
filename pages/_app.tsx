83bfc162526cce581098bba825a5911a20e19e6d
Minsoo Kim <minsooerickim@gmail.com>
2022-08-08 17:03:20 -0700

    add stuff/waiting for stuff to be merged in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,15 @@ import type { AppProps } from 'next/app'
 
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import { UserBar } from '@/components/UserBar/UserBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <UserBar/>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
