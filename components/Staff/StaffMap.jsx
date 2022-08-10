6ae5c72ead339974b9c4b7824b8130b5997a8d19
theo <theobfernandez@gmail.com>
2022-08-10 15:59:20 -0700

    organize StaffMember objects into a map and fix path for Staff.tsx

---
+++ components/Staff/StaffMap.jsx
@@ -0,0 +1,21 @@
+const StaffMap = [
+    {
+            name: "Name",
+            title: "Title",
+            image: "/assets/staffExampleImage.jpg"
+    },
+    {
+            name: "Name",
+            title: "Title",
+            image: "/assets/staffExampleImage.jpg"
+    },
+    {
+            name: "Name",
+            title: "Title",
+            image: "/assets/staffExampleImage.jpg"
+    },
+
+
+
+]
+export default StaffMap
--- pages/sections/Staff.tsx
+++ pages/sections/Staff.tsx
@@ -1,42 +1,27 @@
 import React from 'react'
-import StaffMember from '../../components/Staff/StaffMember'
+import StaffMember from 'components/Staff/StaffMember'
+import StaffMap from "components/Staff/StaffMap.jsx";
+
 
 export default function Staff() {
   return (
     <div className="p-4 flex flex-col justify-center items-center mb-20">
       <h1 className="mb-12">Our Team</h1>
       <div className="flex flex-wrap justify-center">
-        <StaffMember
-          name="Name"
-          title="Title"
-          image="https://images.unsplash.com/photo-1633127603932-d80056a38e9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80"
+        {StaffMap.map((staff) => {
+          return(
+            <>
+              <StaffMember
+              name={staff.name}
+              title={staff.title}
+              image={staff.image}
         />
-        <StaffMember
-          name="Name"
-          title="Title"
-          image="https://images.unsplash.com/photo-1633127603932-d80056a38e9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80"
-        />
-        <StaffMember
-          name="Name"
-          title="Title"
-          image="https://images.unsplash.com/photo-1633127603932-d80056a38e9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80"
-        />
-        <StaffMember
-          name="Name"
-          title="Title"
-          image="https://images.unsplash.com/photo-1633127603932-d80056a38e9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80"
-        />
-        <StaffMember
-          name="Name"
-          title="Title"
-          image="https://images.unsplash.com/photo-1633127603932-d80056a38e9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80"
-        />
-        <StaffMember
-          name="Name"
-          title="Title"
-          image="https://images.unsplash.com/photo-1633127603932-d80056a38e9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80"
-        />
+            </>
+          )
+        })
+      }
+        
       </div>
     </div>
-  )
+  );
 }
