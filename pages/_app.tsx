56fd4f6fe93ea3efddc1a7b1881f291c18fdf42f
Minsoo Kim <minsooerickim@gmail.com>
2022-08-10 19:35:18 -0700

    add userbar in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
+import { UserBar } from '@/components/UserBar/UserBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session} refetchInterval={5 * 60}>
       <Component {...pageProps} />
+      <UserBar />
     </SessionProvider>
   )
 }
