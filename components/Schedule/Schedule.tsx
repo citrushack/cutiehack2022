cb340f527f2d7053687510cb5f4342849c13a721
Nicholas Chao <nibchao@gmail.com>
2022-08-11 21:24:56 -0700

    made formatting somewhat similar to before

--- components/Schedule/Schedule.tsx
+++ components/Schedule/Schedule.tsx
@@ -11,42 +11,45 @@ export default function Schedule() {
         <p>Schedule is in PST (Pacific Standard Time)</p>
         <p>Saturday, April 2</p>
         <div id="Schedule-Wrapper" className="flex flex-row gap-12 px-12 my-6"> 
-          <div className="flex flex-col font-medium">Events</div>
-          {ScheduleItemMap.get('Events').map((scheduleItemInfo) => 
-          {
-            return(
-              <ScheduleItem
-                eventName={scheduleItemInfo.name}
-                date={scheduleItemInfo.date}
-                time={scheduleItemInfo.time}
-                location={scheduleItemInfo.location}
-              />
-            )
-          })}
-          <div className="flex flex-col font-medium">Workshops</div>
-          {ScheduleItemMap.get('Workshops').map((scheduleItemInfo) => 
-          {
-            return(
-              <ScheduleItem
-                eventName={scheduleItemInfo.name}
-                date={scheduleItemInfo.date}
-                time={scheduleItemInfo.time}
-                location={scheduleItemInfo.location}
-              />
-            )
-          })}
-          <div className="flex flex-col font-medium">Activities</div>
-          {ScheduleItemMap.get('Activities').map((scheduleItemInfo) => 
-          {
-            return(
-              <ScheduleItem
-                eventName={scheduleItemInfo.name}
-                date={scheduleItemInfo.date}
-                time={scheduleItemInfo.time}
-                location={scheduleItemInfo.location}
-              />
-            )
-          })}
+          <div className="flex flex-col font-medium w-full px-12 gap-9">Events
+            {ScheduleItemMap.get('Events').map((scheduleItemInfo) => 
+            {
+              return(
+                <ScheduleItem
+                  eventName={scheduleItemInfo.eventName}
+                  date={scheduleItemInfo.date}
+                  time={scheduleItemInfo.time}
+                  location={scheduleItemInfo.location}
+                />
+              )
+            })}
+          </div>
+          <div className="flex flex-col font-medium w-full px-12 gap-9">Workshops
+            {ScheduleItemMap.get('Workshops').map((scheduleItemInfo) => 
+            {
+              return(
+                <ScheduleItem
+                  eventName={scheduleItemInfo.eventName}
+                  date={scheduleItemInfo.date}
+                  time={scheduleItemInfo.time}
+                  location={scheduleItemInfo.location}
+                />
+              )
+            })}
+          </div>
+          <div className="flex flex-col font-medium w-full px-12 gap-9">Activities
+            {ScheduleItemMap.get('Activities').map((scheduleItemInfo) => 
+            {
+              return(
+                <ScheduleItem
+                  eventName={scheduleItemInfo.eventName}
+                  date={scheduleItemInfo.date}
+                  time={scheduleItemInfo.time}
+                  location={scheduleItemInfo.location}
+                />
+              )
+            })}
+          </div>
         </div>
       </section>
     </Page>
--- components/Schedule/ScheduleItem.tsx
+++ components/Schedule/ScheduleItem.tsx
@@ -1,14 +1,14 @@
 export const ScheduleItem = ({eventName, date, time, location}) => {
   return (
     <>
-        <div className="flex flex-col w-full gap-5">
+        <div className="flex flex-row w-full gap-5 items-center">
           <div className="flex flex-col w-full text-left font-medium">
             <div>{time}</div>
             <div>{date}</div>
           </div>
           <div className="flex flex-col w-full text-left">
             <div className="font-medium">{eventName}</div>
-            <div className="italic text-sm">
+            <div className="italic text-sm font-normal">
               {location}
             </div>
           </div>
