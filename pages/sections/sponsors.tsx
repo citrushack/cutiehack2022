c2342a52cc14803dced8bc3c805b60566dedf4db
Raidah Fairooz <raidahfairooz@Raidahs-MacBook-Pro.local>
2022-08-16 17:04:25 -0400

    Created the Sponsor heading and Basic Sponsor Us Button.

--- pages/sections/sponsors.tsx
+++ pages/sections/sponsors.tsx
@@ -1,5 +1,18 @@
+import React, {useState, useEffect} from 'react'
+import { motion } from 'framer-motion'
+
 export default function Sponsors() {
   return (
-    <section className="flex w-full h-screen items-center">Sponsors</section>
+    <div>
+      <div><span className="flex w-full justify-center items-center text-6xl font-bold font-sans">
+      <h1>Our Sponsors</h1></span></div>
+      <div className="py-8 text-lg font-light"><h2>Thank you to all of those who made this hackathon possible!</h2></div>
+      <motion.button 
+      whileHover={{scale: 1.05}}
+      whileTap={{scale: 0.995}}
+      className="flex w-full py-3 justify-center items-center text-xl font-sans font-bold bg-lime-500 rounded-xl">
+      Sponsor Us
+      </motion.button>
+    </div>
   )
 }
