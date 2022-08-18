b7eca52708ac9d35518e353efcc597b11fcf2eeb
Raidah Fairooz <raidahfairooz@Raidahs-MacBook-Pro.local>
2022-08-18 19:37:03 -0400

    fixed formatiing with prettier

--- components/Sponsors/sponsorLogos.tsx
+++ components/Sponsors/sponsorLogos.tsx
@@ -1,52 +1,71 @@
 //import AmazonLogo from '../assets/AmazonSponsor.png'
-interface SponsorLogos{
-    image:string
-    logoKey: string
-    size_width: number
-    size_height: number
-    // logo_link: string -> add later
+interface SponsorLogos {
+  image: string;
+  logoKey: string;
+  size_width: number;
+  size_height: number;
+  // logo_link: string -> add later
 }
 const SponsorLogos = [
-    {image: '/assets/AmazonSponsor.png',
-    logoKey: 'ID1',
+  {
+    image: "/assets/AmazonSponsor.png",
+    logoKey: "ID1",
     size_width: 230,
-    size_height: 230},
-    {image: '/assets/GoogleCloudSponsor.png',
-    logoKey: 'ID2',
+    size_height: 230,
+  },
+  {
+    image: "/assets/GoogleCloudSponsor.png",
+    logoKey: "ID2",
     size_width: 250,
-    size_height: 200},
-    {image: '/assets/AmazonSponsor.png',
-    logoKey: 'ID3',
+    size_height: 200,
+  },
+  {
+    image: "/assets/AmazonSponsor.png",
+    logoKey: "ID3",
     size_width: 200,
-    size_height: 200},
-    {image: '/assets/GoogleCloudSponsor.png',
-    logoKey: 'ID4',
+    size_height: 200,
+  },
+  {
+    image: "/assets/GoogleCloudSponsor.png",
+    logoKey: "ID4",
     size_width: 200,
-    size_height: 170},
-    {image: '/assets/AmazonSponsor.png',
-    logoKey: 'ID5',
+    size_height: 170,
+  },
+  {
+    image: "/assets/AmazonSponsor.png",
+    logoKey: "ID5",
     size_width: 150,
-    size_height: 150},
-    {image: '/assets/GoogleCloudSponsor.png',
-    logoKey: 'ID6',
+    size_height: 150,
+  },
+  {
+    image: "/assets/GoogleCloudSponsor.png",
+    logoKey: "ID6",
     size_width: 150,
-    size_height: 120},
-    {image: '/assets/AmazonSponsor.png',
-    logoKey: 'ID7',
+    size_height: 120,
+  },
+  {
+    image: "/assets/AmazonSponsor.png",
+    logoKey: "ID7",
     size_width: 100,
-    size_height: 100},
-    {image: '/assets/GoogleCloudSponsor.png',
-    logoKey: 'ID8',
+    size_height: 100,
+  },
+  {
+    image: "/assets/GoogleCloudSponsor.png",
+    logoKey: "ID8",
     size_width: 100,
-    size_height: 70},
-    {image: '/assets/AmazonSponsor.png',
-    logoKey: 'ID9',
+    size_height: 70,
+  },
+  {
+    image: "/assets/AmazonSponsor.png",
+    logoKey: "ID9",
     size_width: 100,
-    size_height: 100},
-    {image: '/assets/GoogleCloudSponsor.png',
-    logoKey: 'ID10',
+    size_height: 100,
+  },
+  {
+    image: "/assets/GoogleCloudSponsor.png",
+    logoKey: "ID10",
     size_width: 100,
-    size_height: 70},
-    
-]
-export default SponsorLogos
+    size_height: 70,
+  },
+];
+export default SponsorLogos;
--- pages/sections/sponsors.tsx
+++ pages/sections/sponsors.tsx
@@ -1,38 +1,50 @@
-import React, {useState, useEffect} from 'react'
-import { motion } from 'framer-motion'
-import SponsorLogos from '@/components/Sponsors/SponsorLogos'
-import SponsorMap from '@/components/Sponsors/SponsorMap'
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import SponsorLogos from "@/components/Sponsors/SponsorLogos";
+import SponsorMap from "@/components/Sponsors/SponsorMap";
 
 export default function Sponsors() {
   return (
     <div>
       <div className="flex w-full justify-center items-center text-6xl font-bold font-Arial">
-      <h1>Our Sponsors</h1></div>
+        <h1>Our Sponsors</h1>
+      </div>
       <div className="flex w-full justify-center items-center py-8 text-lg font-light">
-      <h2>Thank you to all of those who made this hackathon possible!</h2></div>
-    
-        <div
-          className="flex justify-center items-center">
-          <motion.button 
-          whileHover={{scale: 1.05}}
-          whileTap={{scale: 0.995}}
-          style= {{display: "flex", width: "200px", height: "3rem", color: "black", fontWeight: "650", backgroundColor: "#87bd08d4",
-          borderRadius: "0.5rem", alignItems: "center", justifyContent: "center" }}>
-          Sponsor Us
-          </motion.button>
-        </div>
+        <h2>Thank you to all of those who made this hackathon possible!</h2>
+      </div>
 
-          <div className="flex flex-wrap h-screen gap-4 items-center justify-center md:gap-8">
-            {SponsorLogos.map((sponsor) => {
-            return (
+      <div className="flex justify-center items-center">
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.995 }}
+          style={{
+            display: "flex",
+            width: "200px",
+            height: "3rem",
+            color: "black",
+            fontWeight: "650",
+            backgroundColor: "#87bd08d4",
+            borderRadius: "0.5rem",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Sponsor Us
+        </motion.button>
+      </div>
+
+      <div className="flex flex-wrap h-screen gap-4 items-center justify-center md:gap-8">
+        {SponsorLogos.map((sponsor) => {
+          return (
             <SponsorMap
               image={sponsor.image}
               key={sponsor.logoKey}
               size_width={sponsor.size_width}
               size_height={sponsor.size_height}
             />
-            )
-            })}
-          </div>
+          );
+        })}
+      </div>
     </div>
-  )}
+  );
+}
