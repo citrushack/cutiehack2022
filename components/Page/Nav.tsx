be6675adc6a09d572d5827c8a40f127ff7a31ef3
Minsoo Kim <minsooerickim@gmail.com>
2022-08-21 14:57:48 -0700

    add navbar

---
+++ components/Page/Nav.tsx
@@ -0,0 +1,51 @@
+import { Link as NavLink } from 'react-scroll'
+import { useRouter } from 'next/router'
+
+export default function Nav() {
+    const hometabs = [
+      {
+        title: 'Home',
+      },
+      {
+        title: 'Schedule',
+      },
+      {
+        title: 'About',
+      },
+      {
+        title: 'Tracks',
+      },
+      {
+        title: 'Support',
+      },
+      {
+        title: 'Sponsors',
+      },
+      {
+        title: 'Staff',
+      },
+      {
+        title: 'FAQ',
+      },
+    ]
+    const router = useRouter()
+    return (
+      <div className="flex place-content-evenly sticky top-3 z-10">
+        {router.pathname === '/' &&
+          hometabs.map(({ title }) => (
+            <span key={title}>
+              <NavLink
+                to={title}
+                className="cursor-pointer rounded-md p-2"
+                smooth={true}
+                spy={true}
+                activeClass="bg-highlight"
+                duration={500}
+              >
+                <span>{title}</span>
+              </NavLink>
+            </span>
+          ))}
+      </div>
+    )
+}
