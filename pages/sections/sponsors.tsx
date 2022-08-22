5af41baf128d269cb8d943101594aa0cfa968dad
Raidah Fairooz <raidahfairooz@Raidahs-MacBook-Pro.local>
2022-08-22 04:56:01 -0400

    added slice method to put logos under specific sections. Added 4 sections of sponsors

--- pages/sections/sponsors.tsx
+++ pages/sections/sponsors.tsx
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
-import SponsorLogos from '@/components/Sponsors/SponsorLogos'
-import SponsorMap from '@/components/Sponsors/SponsorMap'
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import SponsorLogos from "@/components/Sponsors/SponsorLogos";
+import SponsorMap from "@/components/Sponsors/SponsorMap";
+
+const PomeloArray = SponsorLogos.slice(0, 1);
+const OrangeArray = SponsorLogos.slice(1, 2);
+const TangerineArray = SponsorLogos.slice(2, 4);
+const CutieArray = SponsorLogos.slice(4, 11);
 
 export default function Sponsors() {
   return (
@@ -18,23 +23,30 @@ export default function Sponsors() {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.995 }}
           style={{
-            display: 'flex',
-            width: '200px',
-            height: '3rem',
-            color: 'black',
-            fontWeight: '650',
-            backgroundColor: '#87bd08d4',
-            borderRadius: '0.5rem',
-            alignItems: 'center',
-            justifyContent: 'center',
+            display: "flex",
+            width: "200px",
+            height: "3rem",
+            color: "black",
+            fontWeight: "650",
+            backgroundColor: "#87bd08d4",
+            borderRadius: "0.5rem",
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: "1.5rem",
           }}
         >
           Sponsor Us
         </motion.button>
       </div>
-
-      <div className="flex flex-wrap h-screen gap-4 items-center justify-center md:gap-8">
-        {SponsorLogos.map((sponsor) => {
+      {/* Pomelo */}
+      <div
+        style={{ width: "700px" }}
+        className="flex justify-center items-center font-bold font-Arial text-4xl tracking-widest"
+      >
+        Pomelo
+      </div>
+      <div className="flex flex-wrap justify-center items-center">
+        {PomeloArray.map((sponsor) => {
           return (
             <SponsorMap
               image={sponsor.image}
@@ -42,9 +54,66 @@ export default function Sponsors() {
               size_width={sponsor.size_width}
               size_height={sponsor.size_height}
             />
-          )
+          );
+        })}
+      </div>
+      {/* Orange */}
+      <div
+        style={{ width: "700px" }}
+        className="flex justify-center items-center font-bold font-Arial text-4xl tracking-widest"
+      >
+        Orange
+      </div>
+      <div className="flex flex-wrap justify-center items-center my-4">
+        {OrangeArray.map((sponsor) => {
+          return (
+            <SponsorMap
+              image={sponsor.image}
+              key={sponsor.logoKey}
+              size_width={sponsor.size_width}
+              size_height={sponsor.size_height}
+            />
+          );
+        })}
+      </div>
+      {/* Tangerine */}
+      <div
+        style={{ width: "700px" }}
+        className="flex justify-center items-center font-bold font-Arial text-4xl tracking-widest"
+      >
+        Tangerine
+      </div>
+      <div className="flex flex-wrap justify-center items-center my-4">
+        {TangerineArray.map((sponsor) => {
+          return (
+            <SponsorMap
+              image={sponsor.image}
+              key={sponsor.logoKey}
+              size_width={sponsor.size_width}
+              size_height={sponsor.size_height}
+            />
+          );
+        })}
+      </div>
+      {/* Cutie */}
+      <div
+        style={{ width: "700px" }}
+        className="flex justify-center items-center font-bold font-Arial text-4xl tracking-widest"
+      >
+        Cutie
+      </div>
+      <div className="flex flex-wrap justify-center items-center my-4">
+        {CutieArray.map((sponsor) => {
+          return (
+            <SponsorMap
+              image={sponsor.image}
+              key={sponsor.logoKey}
+              size_width={sponsor.size_width}
+              size_height={sponsor.size_height}
+            />
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
