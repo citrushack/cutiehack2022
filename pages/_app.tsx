df350d30770d0886efccbf7d7ce6b4b99171ed0e
Minsoo Kim <minsooerickim@gmail.com>
2022-08-22 18:54:53 -0700

    nav will reside within userbar for now; idk how uiux wants it so this is how it will be for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,11 @@ import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import { UserBar } from '@/components/UserBar/UserBar'
-import Nav from '@/components/Page/Nav'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session} refetchInterval={5 * 60}>
       <ThemeProvider enableSystem={false}>
-        <Nav />
         <Component {...pageProps} />
         <UserBar />
       </ThemeProvider>
