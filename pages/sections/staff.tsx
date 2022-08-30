404386f2dabb981cd65264b3d96aa7aa35ffd6c4
theo <theobfernandez@gmail.com>
2022-08-30 12:57:37 -0700

    run prettier

--- pages/sections/staff.tsx
+++ pages/sections/staff.tsx
@@ -16,9 +16,9 @@ export default function Staff() {
               staffKey={staff.staffKey}
               key={staff.staffKey}
             />
-          );
+          )
         })}
       </div>
     </div>
-  );
+  )
 }
