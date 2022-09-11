584097ba86fde6f0cab34e976860ea1e166af9cf
Nicholas Chao <nibchao@gmail.com>
2022-09-11 13:34:16 -0700

    removed yarn.lock + unused code, switched map from jsx to tsx, removed redundant div

--- components/Schedule/Schedule.tsx
+++ components/Schedule/Schedule.tsx
@@ -1,77 +1,36 @@
-import { Page } from '@/components/Page/Page'
 import React from 'react'
 import { ScheduleItem } from './ScheduleItem'
 import { ScheduleItemMap } from './ScheduleItemMap'
 
 export default function Schedule() {
   return (
-    <Page>
-      <section className="flex flex-col text-center w-full items-center">
-        <h1>Schedule</h1>
-        <p>Schedule is in PST (Pacific Standard Time)</p>
-        <p>Saturday, April 2</p>
-        <div
-          id="Saturday-Schedule-Wrapper"
-          className="flex flex-row gap-12 px-12 my-6"
-        >
-          <div className="flex flex-col font-medium w-full px-12 gap-9">
-            Events
-            {ScheduleItemMap.get('EventsSaturday').map((scheduleItemInfo) => {
-              return (
-                <ScheduleItem
-                  eventName={scheduleItemInfo.eventName}
-                  time={scheduleItemInfo.time}
-                  format={scheduleItemInfo.format}
-                  location={scheduleItemInfo.location}
-                  link={scheduleItemInfo.link}
-                  hover={scheduleItemInfo.hover}
-                />
-              )
-            })}
-          </div>
-          <div className="flex flex-col font-medium w-full px-12 gap-9">
-            Workshops
-            {ScheduleItemMap.get('WorkshopsSaturday').map(
-              (scheduleItemInfo) => {
-                return (
-                  <ScheduleItem
-                    eventName={scheduleItemInfo.eventName}
-                    time={scheduleItemInfo.time}
-                    format={scheduleItemInfo.format}
-                    location={scheduleItemInfo.location}
-                    link={scheduleItemInfo.link}
-                    hover={scheduleItemInfo.hover}
-                  />
-                )
-              }
-            )}
-          </div>
-          <div className="flex flex-col font-medium w-full px-12 gap-9">
-            Activities
-            {ScheduleItemMap.get('ActivitiesSaturday').map(
-              (scheduleItemInfo) => {
-                return (
-                  <ScheduleItem
-                    eventName={scheduleItemInfo.eventName}
-                    time={scheduleItemInfo.time}
-                    format={scheduleItemInfo.format}
-                    location={scheduleItemInfo.location}
-                    link={scheduleItemInfo.link}
-                    hover={scheduleItemInfo.hover}
-                  />
-                )
-              }
-            )}
-          </div>
+    <section className="flex flex-col text-center w-full items-center">
+      <h1>Schedule</h1>
+      <p>Schedule is in PST (Pacific Standard Time)</p>
+      <p>Saturday, April 2</p>
+      <div
+        id="Saturday-Schedule-Wrapper"
+        className="flex flex-row gap-12 px-12 my-6"
+      >
+        <div className="flex flex-col font-medium w-full px-12 gap-9">
+          Events
+          {ScheduleItemMap.get('EventsSaturday').map((scheduleItemInfo) => {
+            return (
+              <ScheduleItem
+                eventName={scheduleItemInfo.eventName}
+                time={scheduleItemInfo.time}
+                format={scheduleItemInfo.format}
+                location={scheduleItemInfo.location}
+                link={scheduleItemInfo.link}
+                hover={scheduleItemInfo.hover}
+              />
+            )
+          })}
         </div>
-        <p>Sunday, April 3</p>
-        <div
-          id="Sunday-Schedule-Wrapper"
-          className="flex flex-row gap-12 px-12 my-6"
-        >
-          <div className="flex flex-col font-medium w-full px-12 gap-9">
-            Events
-            {ScheduleItemMap.get('EventsSunday').map((scheduleItemInfo) => {
+        <div className="flex flex-col font-medium w-full px-12 gap-9">
+          Workshops
+          {ScheduleItemMap.get('WorkshopsSaturday').map(
+            (scheduleItemInfo) => {
               return (
                 <ScheduleItem
                   eventName={scheduleItemInfo.eventName}
@@ -82,39 +41,63 @@ export default function Schedule() {
                   hover={scheduleItemInfo.hover}
                 />
               )
-            })}
-          </div>
-          {/* <div className="flex flex-col font-medium w-full px-12 gap-9">Workshops
-            {ScheduleItemMap.get('WorkshopsSunday').map((scheduleItemInfo) => 
-            {
-              return(
-                <ScheduleItem
-                  eventName={scheduleItemInfo.eventName}
-                  time={scheduleItemInfo.time}
-                  location={scheduleItemInfo.location}
-                  link={scheduleItemInfo.link}
-                  hover={scheduleItemInfo.hover}
-                />
-              )
-            })}
-          </div> */}
-          <div className="flex flex-col font-medium w-full px-12 gap-9">
-            Activities
-            {ScheduleItemMap.get('ActivitiesSunday').map((scheduleItemInfo) => {
-              return (
-                <ScheduleItem
-                  eventName={scheduleItemInfo.eventName}
-                  time={scheduleItemInfo.time}
-                  format={scheduleItemInfo.format}
-                  location={scheduleItemInfo.location}
-                  link={scheduleItemInfo.link}
-                  hover={scheduleItemInfo.hover}
-                />
-              )
-            })}
-          </div>
+            }
+          )}
         </div>
-      </section>
-    </Page>
+        <div className="flex flex-col font-medium w-full px-12 gap-9">
+          Activities
+          {ScheduleItemMap.get('ActivitiesSaturday').map(
+            (scheduleItemInfo) => {
+              return (
+                <ScheduleItem
+                  eventName={scheduleItemInfo.eventName}
+                  time={scheduleItemInfo.time}
+                  format={scheduleItemInfo.format}
+                  location={scheduleItemInfo.location}
+                  link={scheduleItemInfo.link}
+                  hover={scheduleItemInfo.hover}
+                />
+              )
+            }
+          )}
+        </div>
+      </div>
+      <p>Sunday, April 3</p>
+      <div
+        id="Sunday-Schedule-Wrapper"
+        className="flex flex-row gap-12 px-12 my-6"
+      >
+        <div className="flex flex-col font-medium w-full px-12 gap-9">
+          Events
+          {ScheduleItemMap.get('EventsSunday').map((scheduleItemInfo) => {
+            return (
+              <ScheduleItem
+                eventName={scheduleItemInfo.eventName}
+                time={scheduleItemInfo.time}
+                format={scheduleItemInfo.format}
+                location={scheduleItemInfo.location}
+                link={scheduleItemInfo.link}
+                hover={scheduleItemInfo.hover}
+              />
+            )
+          })}
+        </div>
+        <div className="flex flex-col font-medium w-full px-12 gap-9">
+          Activities
+          {ScheduleItemMap.get('ActivitiesSunday').map((scheduleItemInfo) => {
+            return (
+              <ScheduleItem
+                eventName={scheduleItemInfo.eventName}
+                time={scheduleItemInfo.time}
+                format={scheduleItemInfo.format}
+                location={scheduleItemInfo.location}
+                link={scheduleItemInfo.link}
+                hover={scheduleItemInfo.hover}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
--- components/Schedule/ScheduleItem.tsx
+++ components/Schedule/ScheduleItem.tsx
@@ -7,21 +7,16 @@ export const ScheduleItem = ({
   hover,
 }) => {
   return (
-    <>
-      <div className="flex flex-row w-full gap-5 items-center">
-        <div className="flex flex-col w-full text-left font-medium">{time}</div>
-        <div className="flex flex-col w-full text-left">
-          <div className="font-medium">{eventName}</div>
-          <div className="italic text-sm font-normal">
-            <a href={link}>
-              <span className={hover}>
-                <div>{format}</div>
-                <div>{location}</div>
-              </span>
-            </a>
-          </div>
-        </div>
+    <div className="flex flex-row w-full gap-5 items-center">
+      <div className="flex flex-col w-full text-left font-medium">{time}</div>
+      <div className="flex flex-col w-full text-left">
+        <div className="font-medium">{eventName}</div>
+          <a href={link} className="italic text-sm font-normal">
+            <span className={hover}>
+              {format} {location}
+            </span>
+          </a>
       </div>
-    </>
+    </div>
   )
 }
