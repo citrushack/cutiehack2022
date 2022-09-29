6f5df018bb078ee00b38b199fe6d00c2dda08674
Minsoo Kim <minsooerickim@gmail.com>
2022-09-29 01:59:48 -0700

    add css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: '', // or 'media' or 'class'
   theme: {
     screens: {
-      xs: '420px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      'xs': '420px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
       '2xl': '1536px',
     },
     extend: {
+      backgroundImage: {
+        pattern: 'var(--pattern)',
+      },
       colors: {
+        primary: 'var(--primary)',
         secondary: 'var(--secondary)',
+        accent: {
+          DEFAULT: 'var(--accent)',
+          secondary: 'var(--accent-secondary)',
+        },
         card: 'var(--card)',
         overlay: 'var(--overlay)',
         highlight: {
