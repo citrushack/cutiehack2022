e36834d2e748f92aa2a056d3ab6ec5b2680b6682
Minsoo Kim <minsooerickim@gmail.com>
2022-10-01 23:02:31 -0700

    fixed finally

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,9 @@ const Home: NextPage = () => {
     <Page title="Home">
       <Element
         name="Home"
-        className="w-full bg-gradient-to-b from-accent via-accent-secondary to-primary"
+        className="w-full bg-gradient-to-b from-accent via-accent-secondary to-primary "
       >
-        <span className="flex w-full h-screen justify-center bg-[url('/assets/lamps.svg'),_url('/assets/mountains.svg')] bg-[position:top,bottom] bg-contain bg-repeat-x">
+        <span className="flex w-full justify-center bg-[url('/assets/lamps.svg'),_url('/assets/mountains.svg')] bg-[position:top,bottom] bg-contain bg-repeat-x">
           <Landing />
         </span>
       </Element>
