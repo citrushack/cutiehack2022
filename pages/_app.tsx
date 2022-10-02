3401fbd7cdda7ab322ff33b4bf649c100ac06549
Minsoo Kim <minsooerickim@gmail.com>
2022-10-01 22:14:29 -0700

    idk wat to do at this point

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,21 @@ import { ThemeProvider } from 'next-themes'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
-import { UserBar } from '@/components/UserBar/UserBar'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+    console.log('called')
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', documentHeight)
+    documentHeight()
+  })
+
   return (
     <SessionProvider session={pageProps.session} refetchInterval={5 * 60}>
       <ThemeProvider enableSystem={false}>
