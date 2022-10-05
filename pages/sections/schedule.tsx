039d0af15d7639151c7b97a5f5e132eadef39f87
Minsoo Kim <minsooerickim@gmail.com>
2022-10-04 17:07:13 -0700

    add schedule

--- pages/sections/schedule.tsx
+++ pages/sections/schedule.tsx
@@ -1,5 +1,12 @@
+import { MasterSchedule } from '@/components/Schedule/components'
+
 export default function Schedule() {
   return (
-    <section className="flex w-full h-screen items-center">Schedule</section>
+    <section className="flex flex-col w-full h-full my-12 lg:my-0 max-w-[72rem] justify-center items-center">
+      <h1 className="text-center text-4xl font-baloo_semi_bold mb-16">
+        Schedule (PST)
+      </h1>
+      <MasterSchedule />
+    </section>
   )
 }
