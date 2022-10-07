332436cd7f962502c5f5a438d6dc6ced28d34a2e
Minsoo Kim <minsooerickim@gmail.com>
2022-10-07 16:27:59 -0700

    add support section

---
+++ components/SupportCard.tsx
@@ -0,0 +1,32 @@
+import { motion } from 'framer-motion'
+import { ButtonLink } from '@/components/ButtonLink'
+
+interface Props {
+  /** Heading at the top of the card. */
+  title: string
+  /** Description about the way to support the hackathon. */
+  description: string
+  /** Name of the button to diplsay. */
+  buttonLabel: string
+  /** Link to external source. */
+  buttonLink: string
+}
+
+/** Card to display information about ways to support the hackathon. */
+export const SupportCard = ({
+  title,
+  description,
+  buttonLabel,
+  buttonLink,
+}: Props) => (
+  <motion.div
+    whileHover={{ y: -8 }}
+    className={
+      'flex flex-col items-center w-full md:max-w-md p-10 shadow-lg rounded-md cursor-default ' +
+      (title == 'Volunteers' ? 'bg-card' : 'bg-sub-secondary')
+    }
+  >
+    <p className="grow mb-8 text-center text-text font-baloo_regular">{description}</p>
+    <ButtonLink link={buttonLink} label={buttonLabel} external />
+  </motion.div>
+)
--- pages/sections/support.tsx
+++ pages/sections/support.tsx
@@ -1,5 +1,58 @@
+import { SupportCard } from '@/components/SupportCard'
+
 export default function Support() {
+  const cards = [
+    {
+      title: 'Mentors',
+      description:
+        'Not interested in hacking but still want to participate? Great! We would love to have you on board. Fill out the forms below if you’d like to help hackers throughout the day! Not interested in hacking but still want to participate? Great! We would love to have you on board. Fill out the forms below if you’d like to help hackers throughout the day!',
+      buttonLabel: 'Apply',
+      buttonLink: 'https://forms.gle/DDdaTdDrxVierqwi9',
+    },
+    {
+      title: 'Volunteers',
+      description:
+        'Not interested in hacking but still want to participate? Great! We would love to have you on board. Fill out the forms below if you’d like to help hackers throughout the day! Not interested in hacking but still want to participate? Great! We would love to have you on board. Fill out the forms below if you’d like to help hackers throughout the day!',
+      buttonLabel: 'Apply',
+      buttonLink: 'https://forms.gle/vwRa9wkwBdnBXFuFA',
+    },
+    {
+      title: 'Sponsors',
+      description:
+        'Not interested in hacking but still want to participate? Great! We would love to have you on board. Fill out the forms below if you’d like to help hackers throughout the day! Not interested in hacking but still want to participate? Great! We would love to have you on board. Fill out the forms below if you’d like to help hackers throughout the day!',
+      buttonLabel: 'Sponsor',
+      buttonLink: '/citrushack-2022-sponsorship-packet.pdf',
+    },
+  ]
+
   return (
-    <section className="flex w-full h-screen items-center">Support</section>
+    <section className="flex flex-col w-full h-full max-w-[60rem] my-12 lg:mt-0 justify-center items-center pt-10">
+      <h1 className="text-center text-3xl md:text-5xl font-baloo_medium">Get Involved</h1>
+      <p className="mb-10 text-center max-w-lg md:max-w-2xl">
+        Not interested in hacking but still want to participate? Great! We would
+        love to have you on board. Fill out the forms below if you&apos;d like
+        to help hackers throughout the day.
+      </p>
+      <div className="flex flex-col md:flex-row flex-wrap lg:flex-nowrap justify-center gap-6 mb-12">
+        {cards.map(({ title, description, buttonLabel, buttonLink }) => (
+          <div className="flex-col space-y-3" key={title}>
+            <div
+              className={
+                'flex justify-center bg-card rounded-md pt-3 pb-3 items-center ' +
+                (title == 'Volunteers' ? 'bg-card' : 'bg-sub-secondary')
+              }
+            >
+              <h3 className="text-text font-baloo_semi_bold">{title}</h3>
+            </div>
+            <SupportCard
+              title={title}
+              description={description}
+              buttonLabel={buttonLabel}
+              buttonLink={buttonLink}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
