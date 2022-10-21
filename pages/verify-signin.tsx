e7203ee0d4670c3469b5dd1f60f72f00bbaf0936
Minsoo Kim <minsooerickim@gmail.com>
2022-10-21 15:03:00 -0700

    add go back to home button on verify page

--- pages/verify-signin.tsx
+++ pages/verify-signin.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Page } from '@/components/Page/Page'
+import { ButtonLink } from '@/components/ButtonLink'
 
 export default function verifySignIn() {
   return (
     <Page>
       <section className="p-4 flex flex-col justify-center items-center text-center">
         <h1>Check your email</h1>
-        <p>A sign in link has been sent to your email address.</p>
+        <p className='pb-4'>A sign in link has been sent to your email address.</p>
+        <ButtonLink primary label="Go Back to Homepage" link="/" />
       </section>
     </Page>
   )
