c1d90e1734605e0412b5370eb9b3ecc19181b202
NateM135 <melwaninate@gmail.com>
2022-10-21 00:10:54 -0700

    updated email sign-in text

--- pages/verify-signin.tsx
+++ pages/verify-signin.tsx
@@ -5,8 +5,8 @@ export default function verifySignIn() {
   return (
     <Page>
       <section className="p-4 flex flex-col justify-center items-center text-center">
-        <h1>Check your email</h1>
-        <p>A sign in link has been sent to your email address.</p>
+        <h1>Check your email!</h1>
+        <p>A sign-in link has been sent to your email.</p>
       </section>
     </Page>
   )
