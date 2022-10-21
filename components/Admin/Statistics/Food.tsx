376817937a4990c0ab15dfd36c2505209721daf2
NateM135 <melwaninate@gmail.com>
2022-10-21 00:44:38 -0700

    linting files

--- components/Admin/Statistics/Food.tsx
+++ components/Admin/Statistics/Food.tsx
@@ -1,16 +1,16 @@
-import { StatsBlob } from "./Blob";
+import { StatsBlob } from './Blob'
 
 export function FoodStats({ users }) {
   const numMeat = Object.keys(
-    users.filter((user) => user.foodPreference === "Meat")
-  ).length;
+    users.filter((user) => user.foodPreference === 'Meat')
+  ).length
   const numVegetarian = Object.keys(
-    users.filter((user) => user.foodPreference === "Vegetarian")
-  ).length;
+    users.filter((user) => user.foodPreference === 'Vegetarian')
+  ).length
   const numNutAllergy = Object.keys(
-    users.filter((user) => user.foodPreference === "Nut Allergy")
-  ).length;
-  const numTotal = Object.keys(users).length;
+    users.filter((user) => user.foodPreference === 'Nut Allergy')
+  ).length
+  const numTotal = Object.keys(users).length
 
   return (
     <div className="flex flex-col gap-4">
@@ -27,5 +27,5 @@ export function FoodStats({ users }) {
         <StatsBlob num={numNutAllergy} numTotal={numTotal} label="NutAllergy" />
       </div>
     </div>
-  );
+  )
 }
--- components/Form/ApplicationForm/options.tsx
+++ components/Form/ApplicationForm/options.tsx
@@ -1,63 +1,63 @@
-import ExternalLink from "@/components/ExternalLink";
+import ExternalLink from '@/components/ExternalLink'
 
 export const genders = [
-  "Male",
-  "Female",
-  "Nonbinary",
-  "Other",
-  "Prefer not to say",
-];
+  'Male',
+  'Female',
+  'Nonbinary',
+  'Other',
+  'Prefer not to say',
+]
 
 export const ethnicities = [
-  "American Indian or Alaska Native",
-  "Asian",
-  "Black or African American",
-  "Hispanic or Latino",
-  "Native Hawaiian or Other Pacific Islander",
-  "White",
-  "Other",
-  "Prefer not to say",
-];
+  'American Indian or Alaska Native',
+  'Asian',
+  'Black or African American',
+  'Hispanic or Latino',
+  'Native Hawaiian or Other Pacific Islander',
+  'White',
+  'Other',
+  'Prefer not to say',
+]
 
 export const majors = [
-  "Bioengineering",
-  "Chemical Engineering",
-  "Computer Engineering",
-  "Computer Science",
-  "Computer Science and Business Applications",
-  "Data Science",
-  "Electrical Engineering",
-  "Environmental Engineering",
-  "Materials Science and Engineering",
-  "Mechanical Engineering",
-  "Other STEM Major Not Listed",
-  "Humanities Major",
-  "Other Major",
-];
+  'Bioengineering',
+  'Chemical Engineering',
+  'Computer Engineering',
+  'Computer Science',
+  'Computer Science and Business Applications',
+  'Data Science',
+  'Electrical Engineering',
+  'Environmental Engineering',
+  'Materials Science and Engineering',
+  'Mechanical Engineering',
+  'Other STEM Major Not Listed',
+  'Humanities Major',
+  'Other Major',
+]
 
 export const grades = [
-  "High School",
-  "1st Year Undergraduate",
-  "2nd Year Undergraduate",
-  "3rd Year Undergraduate",
-  "4th Year Undergraduate",
-  "5th+ Year Undergraduate",
-  "Graduate",
-  "International",
-];
+  'High School',
+  '1st Year Undergraduate',
+  '2nd Year Undergraduate',
+  '3rd Year Undergraduate',
+  '4th Year Undergraduate',
+  '5th+ Year Undergraduate',
+  'Graduate',
+  'International',
+]
 
-export const firstTimeHacker = ["Yes", "No"];
+export const firstTimeHacker = ['Yes', 'No']
 
-export const participation = ["In-Person", "Online"];
+export const participation = ['In-Person', 'Online']
 
-export const foodPreference = ["Meat", "Vegetarian", "Nut Allergy"];
+export const foodPreference = ['Meat', 'Vegetarian', 'Nut Allergy']
 
-export const shirtSize = ["XS", "S", "M", "L", "XL"];
+export const shirtSize = ['XS', 'S', 'M', 'L', 'XL']
 
 export const MLH = [
   [
     <>
-      I have read and agree to the{" "}
+      I have read and agree to the{' '}
       <ExternalLink
         name="MLH Code of Conduct"
         link="https://github.com/MLH/mlh-policies/blob/master/code-of-conduct.md"
@@ -69,17 +69,17 @@ export const MLH = [
     <>
       I authorize you to share my application/registration information with
       Major League Hacking for event administration, ranking, and MLH
-      administration in-line with the{" "}
+      administration in-line with the{' '}
       <ExternalLink
         name="MLH Privacy Policy"
         link="https://github.com/MLH/mlh-policies/blob/master/privacy-policy.md"
       />
-      . I further agree to the terms of both the{" "}
+      . I further agree to the terms of both the{' '}
       <ExternalLink
         name="MLH Contest Terms and Conditions"
         link="https://github.com/MLH/mlh-policies/blob/master/prize-terms-and-conditions/contest-terms.md"
       />
-      &nbsp;and the{" "}
+      &nbsp;and the{' '}
       <ExternalLink
         name="MLH Privacy Policy"
         link="https://github.com/MLH/mlh-policies/blob/master/privacy-policy.md"
@@ -88,6 +88,6 @@ export const MLH = [
     </>,
   ],
   [
-    "I authorize MLH to send me pre- and post-event informational emails, which contain free credit and opportunities from their partners.",
+    'I authorize MLH to send me pre- and post-event informational emails, which contain free credit and opportunities from their partners.',
   ],
-];
+]
