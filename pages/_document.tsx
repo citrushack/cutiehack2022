e00921b8fcbb12c11551b11d4c3c4a50867f4ddc
Minsoo Kim <minsooerickim@gmail.com>
2022-10-23 12:41:36 -0700

    add custom doc

---
+++ pages/_document.tsx
@@ -0,0 +1,117 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en" className="m-0 p-0 box-border overflow-x-hidden">
+        <Head>
+          <meta
+            name="description"
+            content="Cutie Hack, a 12-hour hackathon hosted at University of California, Riverside."
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="preload" as="image" href="/assets/BigLogo.svg" />
+          <link rel="preload" as="image" href="/assets/sponsors/acm-dark.svg" />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/acm-light.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/google-cloud.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/ieee-dark.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/ieee-light.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/interview-cake-dark.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/interview-cake-light.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/redbull-black.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/redbull-white.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/sketch-dark.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/sketch-light.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/triad-dark.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/triad-light.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/wolfram-dark.svg"
+          />
+          <link
+            rel="preload"
+            as="image"
+            href="/assets/sponsors/wolfram-light.svg"
+          />
+          <link rel="preload" as="image" href="/assets/aboutuslamps.svg" />
+          <link rel="preload" as="image" href="/assets/aboutUsStars.svg" />
+          <link rel="preload" as="image" href="/assets/blurredLogo.png" />
+          <link rel="preload" as="image" href="/assets/desktopLamps.svg" />
+          <link rel="preload" as="image" href="/assets/desktopLampsBlur.png" />
+          <link rel="preload" as="image" href="/assets/hand2.svg" />
+          <link rel="preload" as="image" href="/assets/laptop.svg" />
+          <link rel="preload" as="image" href="/assets/lightbulb.svg" />
+          <link rel="preload" as="image" href="/assets/mobileLamps2.svg" />
+          <link rel="preload" as="image" href="/assets/mobileLampsBlur.png" />
+          <link rel="preload" as="image" href="/assets/mountain2blur.png" />
+          <link rel="preload" as="image" href="/assets/mountains2.svg" />
+          <link rel="preload" as="image" href="/assets/mountains4.svg" />
+          <link rel="preload" as="image" href="/assets/mountains4blur.png" />
+          <link rel="preload" as="image" href="/assets/plants.svg" />
+          <link rel="preload" as="image" href="/assets/staffLineLeft.svg" />
+          <link rel="preload" as="image" href="/assets/staffLineRight.svg" />
+        </Head>
+        <body className="text-text bg-secondary">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
