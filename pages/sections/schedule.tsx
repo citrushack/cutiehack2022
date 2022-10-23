3371c329332c957eba38c2202f231383d1e8ffa6
Minsoo Kim <minsooerickim@gmail.com>
2022-10-22 18:48:36 -0700

    fix overlap issue?

--- pages/sections/schedule.tsx
+++ pages/sections/schedule.tsx
@@ -2,7 +2,7 @@ import { MasterSchedule } from '@/components/Schedule/components'
 
 export default function Schedule() {
   return (
-    <section className="flex flex-col w-full h-full my-12 lg:my-0 max-w-[72rem] justify-center items-center">
+    <section className="flex flex-col w-full h-full my-12 max-w-[72rem] justify-center items-center">
       <h1 className="text-center text-4xl font-baloo_semi_bold mb-16">
         Schedule (PST)
       </h1>
