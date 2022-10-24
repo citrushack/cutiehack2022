b17b6d6f1e024352176d05705499223cf2b840fe
Minsoo Kim <minsooerickim@gmail.com>
2022-10-23 19:22:35 -0700

    run prettier

--- pages/api/users/count-in-person.ts
+++ pages/api/users/count-in-person.ts
@@ -7,7 +7,10 @@ export default async function countUsers(
 ) {
   const db = (await clientPromise).db(process.env.MONGODB_DB)
 
-  const users = await db.collection('users').find({ participation: { $eq: "In-Person" } }).toArray()
+  const users = await db
+    .collection('users')
+    .find({ participation: { $eq: 'In-Person' } })
+    .toArray()
   const numUsersInperson = Object.keys(users).length
 
   res.status(200).json({ numUsersInperson })
