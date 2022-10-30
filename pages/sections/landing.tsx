51f5d48b6bcdab89c5ca79b6e35884397fc8397e
NateM135 <melwaninate@gmail.com>
2022-10-30 12:55:45 -0700

    responsiveness

--- pages/sections/landing.tsx
+++ pages/sections/landing.tsx
@@ -40,7 +40,10 @@ export default function Landing() {
               blurDataURL="/assets/blurredLogo.png"
             />
             <p className="flex font-baloo_semi_bold text-2xl text-highlight justify-center">
-              illuminate your passion - November 5th
+              illuminate your passion
+            </p>
+            <p className="flex font-baloo_semi_bold text-2xl text-highlight justify-center">
+              November 5th
             </p>
           </div>
           <CountdownWrapper date="2022-11-05T16:00:00Z" />
