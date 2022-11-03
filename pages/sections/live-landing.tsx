fe7cdf9097a0ee0e145fcc26c5929acbdf96301e
Minsoo Kim <minsooerickim@gmail.com>
2022-11-02 23:17:40 -0700

    add x padding on live

--- pages/sections/live-landing.tsx
+++ pages/sections/live-landing.tsx
@@ -27,7 +27,7 @@ export default function Landing() {
 
   return (
     <>
-      <section className="relative flex w-full min-safe-h-screen min-h-[48rem] md:min-h-[60rem] xl:max-w-[68rem] 2xl:max-w-[80rem] justify-center items-center my-40 lg:my-0 mb-20 md:mb-0 gap-6 z-30">
+      <section className="relative px-4 flex w-full min-safe-h-screen min-h-[48rem] md:min-h-[60rem] xl:max-w-[68rem] 2xl:max-w-[80rem] justify-center items-center my-40 lg:my-0 mb-20 md:mb-0 gap-6 z-30">
         <div className="lg:absolute flex flex-col left-4 2xl:left-0 items-center">
           <div className="flex flex-col max-w-xl justify-center">
             {/* <h1 className="font-baloo_bold text-5xl">CUTIE HACK</h1> */}
